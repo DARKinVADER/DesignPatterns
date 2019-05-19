a9e6acda3266606c94f32832cbd8d9989c10b034
Gábor Czibik <gabor@czibik.hu>
2019-05-19 20:44:26 +0200

    Singleton Basic example

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton singleton = Singleton.GetInstance();
+            Console.WriteLine($"Hello World of Singleton, Counter is {singleton.Counter++}!");
+            Console.WriteLine($"Hello World of Singleton, Counter is incremented by one : {singleton.Counter}!");
+
+            Singleton singletonSecond = Singleton.GetInstance();
+            Console.WriteLine($"Singleton - singletonSecond, Counter is {singletonSecond.Counter}!");
+        }
+    }
+}
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private static Singleton uniqueInstance;
+        public int Counter { get; set; }
+
+        private Singleton()
+        {
+            Counter = 1;
+        }
+
+        public static Singleton GetInstance()
+        {
+            if(uniqueInstance== null)
+            {
+                uniqueInstance = new Singleton();
+            }
+            return uniqueInstance;
+        }
+    }
+}

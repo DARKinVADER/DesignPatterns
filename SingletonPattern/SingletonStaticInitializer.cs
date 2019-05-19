b316071b07ab22c869356f3a1f706bc9fa580333
Gábor Czibik <gabor@czibik.hu>
2019-05-19 21:03:03 +0200

    Singleton Pattern - Static Initializer

---
+++ SingletonPattern/SingletonStaticInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    // For performance issues
+    public class SingletonStaticInitializer
+    {
+        private static SingletonStaticInitializer uniqueInstance = new SingletonStaticInitializer();
+        private SingletonStaticInitializer()
+        {
+        }
+
+        public static SingletonStaticInitializer GetInstance()
+        {
+            return uniqueInstance;
+        }
+    }
+}
